66472c0d2ed945c2d23d70ddb89d948a3e0e53f7
Sushank Gurung <jagatgurung30@gmail.com>
2025-11-01 16:58:01 +0545

    implement posthog

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import LightRays from "@/components/LightRays";
 
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import { PostHogProvider, PostHogPageView } from "./providers";
 
 const schibstedGrotesk = Schibsted_Grotesk({
   variable: "--font-schibsted-grotesk",
@@ -30,23 +31,26 @@ export default function RootLayout({
       <body
         className={`${schibstedGrotesk.variable} ${martianMono.variable} min-h-screen  antialiased`}
       >
-        <Navbar />
-        <div className="absolute inset-0 top-0 z-[-1] min-h-screen">
-          <LightRays
-            raysOrigin="top-center-offset"
-            raysColor="#5dfeca"
-            raysSpeed={0.5}
-            lightSpread={0.9}
-            rayLength={1.4}
-            followMouse={true}
-            mouseInfluence={0.02}
-            noiseAmount={0.0}
-            distortion={0.01}
-            className="custom-rays"
-          />
-        </div>
+        <PostHogProvider>
+          <PostHogPageView />
+          <Navbar />
+          <div className="absolute inset-0 top-0 z-[-1] min-h-screen">
+            <LightRays
+              raysOrigin="top-center-offset"
+              raysColor="#5dfeca"
+              raysSpeed={0.5}
+              lightSpread={0.9}
+              rayLength={1.4}
+              followMouse={true}
+              mouseInfluence={0.02}
+              noiseAmount={0.0}
+              distortion={0.01}
+              className="custom-rays"
+            />
+          </div>
 
-        <main>{children}</main>
+          <main>{children}</main>
+        </PostHogProvider>
       </body>
     </html>
   );
